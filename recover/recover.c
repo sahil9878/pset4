fe4ffb59496327a6a9ec30f78ebfbce97962a495
Sahil <s.sahil3024@gmail.com>
2017-05-14 03:49:05 +0000

    freed up memory also passes style50

--- recover/recover.c
+++ recover/recover.c
@@ -11,9 +11,9 @@ int main(int argc, char* argv[])
     }
     
     // Opening the card file in read mode
-    FILE *fp = fopen(argv[1],"r");
+    FILE *fptr = fopen(argv[1],"r");
 
-    if (fp == NULL)
+    if (fptr == NULL)
     {
         fprintf (stderr,"Error! file %s can't be opened\n",argv[1]);
         return 2;
@@ -24,30 +24,30 @@ int main(int argc, char* argv[])
     // Declaring a buffer of size 512
     unsigned char buffer[512];
  
-     // To read 512 blocks in one go and doing it till we get EOF
-    while ( fread(buffer, sizeof(unsigned char), 512, fp) == 512) 
+     // To read 512 blocks in one go 
+    while ( fread(buffer, sizeof(unsigned char), 512, fptr) == 512) 
     {
         // To detect JPEG's signature
         if ((buffer[0] == 0xff) && 
             (buffer[1] == 0xd8) && 
             (buffer[2] == 0xff) && 
             ((buffer[3] & 0xf0) == 0xe0)) 
-            {
+        {
             if (count != 0)
             {
                 fclose(pic);
             }
             
-            // Declaring a string to determmine JPEG's name
+            // String to determmine JPEG's name
             
             char filename[10];
             sprintf(filename, "%03i.jpg", count);
             
-            // Increment count by 1 on detecting a JPEG
+            // Increment count by 1 for a JPEG
             
             count++;
             
-             // Opening a new file in write mode to place JPEG's pixel
+             // Opening a new file in write mode to place JPEG's pixels
              
             pic = fopen(filename, "w");
             if (pic == NULL) 
@@ -61,11 +61,12 @@ int main(int argc, char* argv[])
             fwrite(buffer, sizeof(unsigned char), 512, pic);
             
     }
-      // If the old JPEG's pixel's aren't over yet
+      // If the old JPEG's pixels aren't over yet
         else if (pic != NULL) 
         {
             fwrite(buffer, sizeof(unsigned char), 512, pic);
         }
         }
-
-}
+         // closing file
+    fclose(fptr);
+} 
