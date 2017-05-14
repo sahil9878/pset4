f1f951c6b9ba4a5e7e7edc8e2684975f6d63e612
Sahil <s.sahil3024@gmail.com>
2017-05-14 03:39:25 +0000

    program detects and creates 50 JPEGs but all are blank

--- recover/recover.c
+++ recover/recover.c
@@ -18,9 +18,12 @@ int main(int argc, char* argv[])
         fprintf (stderr,"Error! file %s can't be opened\n",argv[1]);
         return 2;
     }
+    // Declare a file pointer to place the JPEG's pixel in new file
+    FILE* pic = NULL;
+    int count = 0;
     // Declaring a buffer of size 512
     unsigned char buffer[512];
-   int count = 0; 
+ 
      // To read 512 blocks in one go and doing it till we get EOF
     while ( fread(buffer, sizeof(unsigned char), 512, fp) == 512) 
     {
@@ -29,9 +32,36 @@ int main(int argc, char* argv[])
             (buffer[1] == 0xd8) && 
             (buffer[2] == 0xff) && 
             ((buffer[3] & 0xf0) == 0xe0)) 
-              count++;  
+            {
+            if (count != 0)
+            {
+                fclose(pic);
+            }
+            
+            // Declaring a string to determmine JPEG's name
+            
+            char filename[10];
+            sprintf(filename, "%03i.jpg", count);
+            
+            // Increment count by 1 on detecting a JPEG
+            
+            count++;
+            
+             // Opening a new file in write mode to place JPEG's pixel
+             
+            pic = fopen(filename, "w");
+            if (pic == NULL) 
+            {
+                fprintf(stderr,"Error! Can't write in file %s\n",filename);
+                return 3;
+            }
+            
+            // Writing to the file 512 bytes in one go
+            
+            fwrite(buffer, sizeof(unsigned char), 512, pic);
+            
     }
-    {printf("%d jpeg found\n",count);
+    
         }
    
 
