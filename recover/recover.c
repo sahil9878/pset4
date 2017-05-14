b483e95ac92948bfcee9286789b703df93c6e4ef
Sahil <s.sahil3024@gmail.com>
2017-05-14 03:32:08 +0000

    program detects the presence of 50 JPEGs

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
 
-
 int main(int argc, char* argv[])
 {
     // Ensure proper usage
@@ -19,5 +18,21 @@ int main(int argc, char* argv[])
         fprintf (stderr,"Error! file %s can't be opened\n",argv[1]);
         return 2;
     }
+    // Declaring a buffer of size 512
+    unsigned char buffer[512];
+   int count = 0; 
+     // To read 512 blocks in one go and doing it till we get EOF
+    while ( fread(buffer, sizeof(unsigned char), 512, fp) == 512) 
+    {
+        // To detect JPEG's signature
+        if ((buffer[0] == 0xff) && 
+            (buffer[1] == 0xd8) && 
+            (buffer[2] == 0xff) && 
+            ((buffer[3] & 0xf0) == 0xe0)) 
+              count++;  
+    }
+    {printf("%d jpeg found\n",count);
+        }
+   
+
 }
-    
